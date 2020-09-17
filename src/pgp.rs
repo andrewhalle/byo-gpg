f31411e7ae8f90f1595b9ee0300e7621129a280d
Andrew Halle <ahalle@berkeley.edu>
2020-09-16 19:57:54 -0700

    implement checking the crc24 checksum at the end of the base64 data

--- src/pgp.rs
+++ src/pgp.rs
@@ -1,20 +1,36 @@
+use nom::bytes::complete::take_while_m_n;
 use nom::bytes::complete::{tag, take_till, take_until, take_while};
 use nom::character::complete::alphanumeric1;
 use nom::character::complete::newline;
+use nom::multi::fold_many0;
 use nom::multi::many0;
 use nom::sequence::{preceded, terminated};
 use nom::IResult;
+
+const BASE64_LINE_LENGTH: usize = 64_usize;
+const CRC24_INIT: u32 = 0xB704CE;
+const CRC24_POLY: u32 = 0x1864CFB;
 
 #[derive(Debug)]
 pub struct CleartextSignature {
     hash: Option<String>,
     cleartext: String,
-    signature: String,
-    end_bytes: String, // what is this field for?
+    signature: PgpSignature,
 }
 
-// XXX fix me, for now just stuff the base64 bytes into this vec.
-// type PgpSignature = Vec<u8>;
+#[derive(Debug)]
+struct PgpSignature {
+    data: Vec<u8>,
+    checksum: Vec<u8>,
+    valid_state: ValidState,
+}
+
+#[derive(Debug, PartialEq)]
+enum ValidState {
+    Unchecked,
+    Valid,
+    Invalid,
+}
 
 impl CleartextSignature {
     pub fn parse_from(data: &str) -> Result<CleartextSignature, &'static str> {
@@ -26,8 +42,8 @@ impl CleartextSignature {
 
         let (input, cleartext) = parse_cleartext(input).map_err(|_: nom::Err<(_, _)>| "error 3")?;
 
-        let (_input, (signature, end_bytes)) =
-            parse_pgp_signature(input).map_err(|_: nom::Err<(_, _)>| "error 4")?;
+        let (_input, mut signature) =
+            PgpSignature::parse(input).map_err(|_: nom::Err<(_, _)>| "error 4")?;
 
         // assert end of file here using all_consuming
 
@@ -37,15 +53,52 @@ impl CleartextSignature {
         };
         let cleartext = cleartext.to_string().replace("\n- ", "\n");
 
-        let mut signature = signature.to_string();
-        signature.retain(|c| c != '\n');
+        signature.validate();
+        if signature.valid_state == ValidState::Invalid {
+            Err("signature not validated")?;
+        }
 
         Ok(CleartextSignature {
             hash: Some(hash.to_string()),
             cleartext,
             signature,
-            end_bytes: end_bytes.to_string(),
         })
+    }
+}
+
+impl PgpSignature {
+    fn parse(input: &str) -> IResult<&str, PgpSignature> {
+        let (input, _) = tag("-----BEGIN PGP SIGNATURE-----\n")(input)?;
+        let (input, _) = newline(input)?;
+        let (input, base64) = parse_base64(input)?;
+        let (input, _) = take_while(|c| c == '\n' || c == '=')(input)?;
+        let (input, checksum) = take_till(|c| c == '\n')(input)?;
+        let (input, _) = newline(input)?;
+        let (input, _) = tag("-----END PGP SIGNATURE-----\n")(input)?;
+
+        let data = base64::decode(&base64).unwrap();
+        let checksum = base64::decode(&checksum).unwrap();
+
+        Ok((
+            input,
+            PgpSignature {
+                data,
+                checksum,
+                valid_state: ValidState::Unchecked,
+            },
+        ))
+    }
+
+    fn validate(&mut self) {
+        let checksum_computed = crc24(self.data.as_slice());
+        let checksum_stored = (self.checksum[0] as u32) << 16
+            | (self.checksum[1] as u32) << 8
+            | (self.checksum[2] as u32);
+        self.valid_state = if checksum_computed == checksum_stored {
+            ValidState::Valid
+        } else {
+            ValidState::Invalid
+        }
     }
 }
 
@@ -60,14 +113,42 @@ fn parse_cleartext(input: &str) -> IResult<&str, &str> {
     Ok((left, cleartext))
 }
 
-fn parse_pgp_signature(input: &str) -> IResult<&str, (&str, &str)> {
-    let (input, _) = tag("-----BEGIN PGP SIGNATURE-----\n")(input)?;
+// XXX there's a bug in this, if the base64 data is exactly as long as a line, then it
+// won't recognize the end.
+fn parse_base64(input: &str) -> IResult<&str, String> {
+    let (input, mut base64) = fold_many0(parse_base64_line, String::new(), |mut s, item| {
+        s.push_str(item);
+        s
+    })(input)?;
+    let (input, remaining) = take_till(|c| c == '\n')(input)?;
     let (input, _) = newline(input)?;
-    let (input, base64) = take_until("==")(input)?;
-    let (input, _) = take_while(|c| c == '\n' || c == '=')(input)?;
-    let (input, end_field) = take_till(|c| c == '\n')(input)?;
-    let (input, _) = newline(input)?;
-    let (input, _) = tag("-----END PGP SIGNATURE-----\n")(input)?;
 
-    Ok((input, (base64, end_field)))
+    base64.push_str(remaining);
+
+    Ok((input, base64))
+}
+
+fn parse_base64_line(input: &str) -> IResult<&str, &str> {
+    let (input, res) =
+        take_while_m_n(BASE64_LINE_LENGTH, BASE64_LINE_LENGTH, |c| c != '\n')(input)?;
+    let (input, _) = newline(input)?;
+
+    Ok((input, res))
+}
+
+/// Implementation of CRC24 directly from the RFC.
+/// https://tools.ietf.org/html/rfc4880#section-6.1
+fn crc24(data: &[u8]) -> u32 {
+    let mut crc = CRC24_INIT;
+    for curr in data.iter() {
+        crc ^= (*curr as u32) << 16;
+        for _ in 0..8 {
+            crc <<= 1;
+            if crc & 0x1000000 != 0 {
+                crc ^= CRC24_POLY;
+            }
+        }
+    }
+
+    crc & 0xFFFFFF
 }
